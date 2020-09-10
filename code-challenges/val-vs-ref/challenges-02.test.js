3d5084ee19ef138f138b61d1bf221abc8d471b6b
Doktor-Doom <andre.o.martin@gmail.com>
2020-09-09 23:43:12 -0700

    resub

--- code-challenges/val-vs-ref/challenges-02.test.js
+++ code-challenges/val-vs-ref/challenges-02.test.js
@@ -27,8 +27,9 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   let otherString = `${str} The end.`;
-  return otherString;
+
   // Solution code here...
+  return otherString;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -46,6 +47,8 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+  let eerstDing = arr[0];
+  arr.push(eerstDing);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -65,6 +68,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
