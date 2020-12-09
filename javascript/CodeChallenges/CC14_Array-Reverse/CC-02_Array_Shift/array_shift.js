cffddde13f0b426e81cd3db18f2fee32fb4a269b
Doktor-Doom <andre.o.martin@gmail.com>
2020-12-08 20:49:55 -0800

    CC2

--- javascript/CodeChallenges/CC14_Array-Reverse/CC-02_Array_Shift/array_shift.js
+++ javascript/CodeChallenges/CC14_Array-Reverse/CC-02_Array_Shift/array_shift.js
@@ -2,8 +2,17 @@
 
 function insertShiftArray (arr, num) {
   let index = Math.ceil(arr.length/2);
-  arr[index] = num;
-  return arr;
+  let banana = [];
+  for (let i = 0; i < arr.length + 1; i++) {
+    if (i < index) {
+      banana[i] = arr[i];
+    } else if (i === index) {
+      banana[i] = num;
+    } else {
+      banana[i] = arr[i - 1];
+    }
+  };
+  return banana;
 };
 
 insertShiftArray();
