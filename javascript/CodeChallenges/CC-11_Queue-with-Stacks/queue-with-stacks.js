b767547c3e8f9295d3202ef1c380161a520cc2ec
Doktor-Doom <andre.o.martin@gmail.com>
2020-12-22 02:58:29 -0500

    mm

---
+++ javascript/CodeChallenges/CC-11_Queue-with-Stacks/queue-with-stacks.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class PseudoQueue {
+  constructor() {
+    this.front = new Stack();
+    this.rear = new Stack();
+  }
+  enqueue(val) {
+
+  }
+  // this.front.push(val);
+
+  dequeue() {
+    this.front.pop;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+
+  }
+  push
+}
