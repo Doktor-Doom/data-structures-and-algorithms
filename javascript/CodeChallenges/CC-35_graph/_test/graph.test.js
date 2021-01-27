cf7cb7d3333e11a0b4b7199d3bf08ea74a751e95
Doktor-Doom <andre.o.martin@gmail.com>
2021-01-26 23:53:33 -0800

    partial whiteboard for 37

--- javascript/CodeChallenges/CC-35_graph/_test/graph.test.js
+++ javascript/CodeChallenges/CC-35_graph/_test/graph.test.js
@@ -1,2 +1,12 @@
 'use strict';
+const {Graph, Vertex} = require('./test/graph');
 
+describe('Graph data structure', ()=>{
+
+  it('1. Node can be successfully added to the graph', ()=>{
+    let vertex = new Vertex(7);
+    let graph = new Graph();
+    graph.addVertex(vertex);
+    console.log('1. node added to graph', graph)
+    expect(graph.getNeighbors(vertex)).toEqual([]);
+  })
