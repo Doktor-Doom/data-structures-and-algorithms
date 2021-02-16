baa65310b093ef151689c65bb343cda3c5c8bff7
Doktor-Doom <andre.o.martin@gmail.com>
2021-02-15 21:51:13 -0500

    CC 35 - 38

--- javascript/CodeChallenges/CC-35_graph/_test/graph.test.js
+++ javascript/CodeChallenges/CC-35_graph/_test/graph.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const {Graph, Vertex} = require('./test/graph');
+const {Graph, Vertex} = require('./graph');
 
 describe('Graph data structure', ()=>{
 
@@ -9,4 +9,22 @@ describe('Graph data structure', ()=>{
     graph.addVertex(vertex);
     console.log('1. node added to graph', graph)
     expect(graph.getNeighbors(vertex)).toEqual([]);
-  })
+  });
+
+  it('Can successfully return a graph with only one node', () => {
+    expect(graph.getNodes().get(vertex1)).toStrictEqual([]);
+  });
+
+  it('Can successfully return a graph with only 1 edge (two nodes and an edge)', () => {
+    expect(graph.size()).toBe(2);
+  });
+
+  it('Can successfully return weights of the edges of the neighbors of a node in the graph', () => {
+    let neighbors = graph.getNeighbors(vertex4);
+    expect(neighbors.length).toBe(2);
+    expect(neighbors[0].weight).toStrictEqual(5);
+    expect(neighbors[1].weight).toStrictEqual(5);
+
+  });
+
+};
